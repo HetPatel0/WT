b95d5aebe26f73e18916f0f5de95b9765ebd48b5
HetPatel0 <142023310+HetPatel0@users.noreply.github.com>
2024-09-01 21:29:51 +0530

    express learning

---
+++ wt work/express/first.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const http = require("http")
+const app = express();
+app.get('/',(req,res)=>{
+    return res.send("Hello from home page");
+})
+
+app.get('/about',(req,res)=>{
+    return res.send ("hello from about page");
+})
+
+const myServer  = http.createServer(app);
+myServer.listen(3000,()=>console.log("server started" ));
